ff16fb20a1284e5a82ff9502bf8e2b29c2a3b840
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2025-12-10 07:51:19 +0300

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -36,25 +36,13 @@ define(['managerAPI',
         whiteLabels:whiteLabels,
         //Select randomly what attribute words to see. 
         //Based on Axt, Feng, & Bar-Anan (2021).
-        posWords : API.shuffle([
-            'Love', 'Cheer', 'Friend', 'Pleasure',
-            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
-            'Smiling','Cherish', 'Excellent', 'Glad', 
-            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
-            'Excitement', 'Laughing', 'Attractive','Delightful', 
-            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
-            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
-            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph'
-        ]), 
-        negWords : API.shuffle([
-            'Abuse', 'Grief', 'Poison', 'Sadness', 
-            'Pain', 'Despise', 'Failure', 'Nasty', 
-            'Angry', 'Detest', 'Horrible', 'Negative', 
-            'Ugly', 'Dirty', 'Gross', 'Evil', 
-            'Rotten','Annoy', 'Disaster', 'Horrific',  
-            'Scorn', 'Awful', 'Disgust', 'Hate', 
-            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
-            'Hatred', 'Hurtful', 'Sickening', 'Yucky'
+        baWords : API.shuffle([
+            'Башкортостан', 'Сабантуй', 'Бешбармак', 'Урал-Батыр',
+            'Уфа', 'Агидель', 'Бешмет', 'Курай'
+        ]),
+        ruWords : API.shuffle([
+            'Россия', 'Масленица', 'Пельмени', 'Илья Муромец', 
+            'Москва', 'Енисей', 'Кокошник', 'Балалайка'
         ])
     });
 
